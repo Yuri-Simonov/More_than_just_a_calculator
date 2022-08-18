955460355cf438887975ac93de81e9ea5b2d0aa7
Yuri Simonov <yura15-96@mail.ru>
2022-08-18 16:48:24 +0300

    поправлены заголовки в секциях

--- src/app/components/pages/sections/sections.jsx
+++ src/app/components/pages/sections/sections.jsx
@@ -16,26 +16,48 @@ import weightLogo from "../../../images/svg/weight.svg";
 
 const Sections = () => {
     const allSections = [
-        { logo: dataLogo, link: "digital_data", alt: "digital_data" },
+        {
+            logo: dataLogo,
+            link: "digital_data",
+            alt: "digital_data",
+            title: "Данные"
+        },
         {
             logo: bodyWeightMeterLogo,
             link: "body_weight_meter",
-            alt: "body_weight_meter"
+            alt: "body_weight_meter",
+            title: "ИМТ"
         },
-        { logo: ageLogo, link: "body_weight_meter", alt: "body_weight_meter" },
-        { logo: discountLogo, link: "discount", alt: "discount" },
-        { logo: dateLogo, link: "date", alt: "date" },
-        { logo: lengthLogo, link: "length", alt: "length" },
-        { logo: squareLogo, link: "square", alt: "square" },
-        { logo: volumeLogo, link: "volume", alt: "volume" },
-        { logo: temperatureLogo, link: "temperature", alt: "temperature" },
-        { logo: speedLogo, link: "speed", alt: "speed" },
-        { logo: timeLogo, link: "time", alt: "time" },
-        { logo: weightLogo, link: "weight", alt: "weight" },
+        {
+            logo: ageLogo,
+            link: "body_weight_meter",
+            alt: "body_weight_meter",
+            title: "Возраст"
+        },
+        {
+            logo: discountLogo,
+            link: "discount",
+            alt: "discount",
+            title: "Скидка"
+        },
+        { logo: dateLogo, link: "date", alt: "date", title: "Дата" },
+        { logo: lengthLogo, link: "length", alt: "length", title: "Длина" },
+        { logo: squareLogo, link: "square", alt: "square", title: "Площадь" },
+        { logo: volumeLogo, link: "volume", alt: "volume", title: "Объем" },
+        {
+            logo: temperatureLogo,
+            link: "temperature",
+            alt: "temperature",
+            title: "Температура"
+        },
+        { logo: speedLogo, link: "speed", alt: "speed", title: "Скорость" },
+        { logo: timeLogo, link: "time", alt: "time", title: "Время" },
+        { logo: weightLogo, link: "weight", alt: "weight", title: "Масса" },
         {
             logo: scaleOfNotationLogo,
             link: "scale_of_notation",
-            alt: "scale_of_notation"
+            alt: "scale_of_notation",
+            title: "Система счисления"
         }
     ];
 
@@ -55,7 +77,7 @@ const Sections = () => {
                                             />
                                         </div>
                                         <h1 className="sections__item-title">
-                                            Данные
+                                            {section.title}
                                         </h1>
                                     </article>
                                 </Link>
