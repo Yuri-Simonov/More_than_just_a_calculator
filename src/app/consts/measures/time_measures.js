7d00f4084f9a21b2dae9e39bc53da018cb7f4f32
Yuri Simonov <yura15-96@mail.ru>
2022-08-23 11:35:33 +0300

    реализована подсекция времени

---
+++ src/app/consts/measures/time_measures.js
@@ -0,0 +1,11 @@
+export const timeMeasures = [
+    { shortName: "y", fullName: "Годы", size: 1 },
+    { shortName: "wk", fullName: "Недели", size: 0.0191781 },
+    { shortName: "d", fullName: "Дни", size: 0.00273785 },
+    { shortName: "h", fullName: "Часы", size: 0.000114077 },
+    { shortName: "min", fullName: "Минуты", size: 1.90129e-6 },
+    { shortName: "s", fullName: "Секунды", size: 3.171e-8 },
+    { shortName: "ms", fullName: "Миллисекунды", size: 3.171e-11 },
+    { shortName: "μs", fullName: "Микросекунды", size: 3.171e-14 },
+    { shortName: "ps", fullName: "Наносекунды", size: 3.171e-17 }
+];
