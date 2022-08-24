88fb535bd7a969b518a8c6c07f8da135adb7dfcf
Yuri Simonov <yura15-96@mail.ru>
2022-08-24 12:01:26 +0300

    реализована подсекция скидки

--- src/app/components/common/conversionTwoOperators.jsx
+++ src/app/components/common/conversionTwoOperators.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { round } from "mathjs";
 
 const ConversionTwoOperators = ({
     measures,
@@ -9,30 +10,39 @@ const ConversionTwoOperators = ({
     firstResultValue,
     secondResultValue,
     toggleActiveField,
-    activeField
+    activeField,
+    methodOfCalculation,
+    finalResult
 }) => {
     return (
         <section className="conversion">
             <div className="conversion__body">
                 <div className="conversion__item">
-                    <div className="conversion__select">
-                        <select
-                            name="first"
-                            id="first"
-                            value={firstSelectValue.shortName}
-                            onChange={(e) =>
-                                toggleSelect(e.target.id, e.target.value)
-                            }
-                        >
-                            {measures[0].map((meas, i) => {
-                                return (
-                                    <option key={i} value={meas.shortName}>
-                                        {meas.fullName}
-                                    </option>
-                                );
-                            })}
-                        </select>
-                    </div>
+                    {methodOfCalculation === "bodyWeightMeter" && (
+                        <div className="conversion__select">
+                            <select
+                                name="first"
+                                id="first"
+                                value={firstSelectValue.shortName}
+                                onChange={(e) =>
+                                    toggleSelect(e.target.id, e.target.value)
+                                }
+                            >
+                                {measures[0].map((meas, i) => {
+                                    return (
+                                        <option key={i} value={meas.shortName}>
+                                            {meas.fullName}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                        </div>
+                    )}
+                    {methodOfCalculation === "discount" && (
+                        <p className="conversion__single-measures">
+                            Первоначальная цена
+                        </p>
+                    )}
                     <div className="conversion__value">
                         <p
                             className={
@@ -44,30 +54,39 @@ const ConversionTwoOperators = ({
                         >
                             {firstResultValue}
                         </p>
-                        <p className="conversion__measure">
-                            {firstSelectValue.fullName}
-                        </p>
+                        {methodOfCalculation === "bodyWeightMeter" && (
+                            <p className="conversion__measure">
+                                {firstSelectValue.fullName}
+                            </p>
+                        )}
                     </div>
                 </div>
                 <div className="conversion__item">
-                    <div className="conversion__select">
-                        <select
-                            name="second"
-                            id="second"
-                            value={secondSelectValue.shortName}
-                            onChange={(e) =>
-                                toggleSelect(e.target.id, e.target.value)
-                            }
-                        >
-                            {measures[1].map((meas, i) => {
-                                return (
-                                    <option key={i} value={meas.shortName}>
-                                        {meas.fullName}
-                                    </option>
-                                );
-                            })}
-                        </select>
-                    </div>
+                    {methodOfCalculation === "bodyWeightMeter" && (
+                        <div className="conversion__select">
+                            <select
+                                name="second"
+                                id="second"
+                                value={secondSelectValue.shortName}
+                                onChange={(e) =>
+                                    toggleSelect(e.target.id, e.target.value)
+                                }
+                            >
+                                {measures[1].map((meas, i) => {
+                                    return (
+                                        <option key={i} value={meas.shortName}>
+                                            {meas.fullName}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                        </div>
+                    )}
+                    {methodOfCalculation === "discount" && (
+                        <p className="conversion__single-measures">
+                            Скидка (%)
+                        </p>
+                    )}
                     <div className="conversion__value">
                         <p
                             className={
@@ -79,25 +98,47 @@ const ConversionTwoOperators = ({
                         >
                             {secondResultValue}
                         </p>
-                        <p className="conversion__measure">
-                            {secondSelectValue.fullName}
-                        </p>
+                        {methodOfCalculation === "bodyWeightMeter" && (
+                            <p className="conversion__measure">
+                                {secondSelectValue.fullName}
+                            </p>
+                        )}
                     </div>
                 </div>
+                {methodOfCalculation === "discount" && (
+                    <div className="conversion__item">
+                        <p className="conversion__single-measures">
+                            Окончательная цена
+                        </p>
+                        <div className="conversion__value">
+                            <p className="conversion__result">{finalResult}</p>
+                        </div>
+                    </div>
+                )}
+                {methodOfCalculation === "discount" && (
+                    <div className="conversion__prompt">
+                        <p>
+                            Вы сэкономите{" "}
+                            {round(firstResultValue - finalResult, 5)}
+                        </p>
+                    </div>
+                )}
             </div>
         </section>
     );
 };
 
 ConversionTwoOperators.propTypes = {
-    measures: PropTypes.array.isRequired,
+    measures: PropTypes.array,
     toggleSelect: PropTypes.func,
     toggleActiveField: PropTypes.func,
     firstSelectValue: PropTypes.object,
     secondSelectValue: PropTypes.object,
     firstResultValue: PropTypes.string,
     secondResultValue: PropTypes.string,
-    activeField: PropTypes.number
+    methodOfCalculation: PropTypes.string,
+    activeField: PropTypes.number,
+    finalResult: PropTypes.number
 };
 
 export default ConversionTwoOperators;
--- src/app/components/pages/sections/discount.jsx
+++ src/app/components/pages/sections/discount.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import SimplePanel from "../../common/simple_panel";
+import Title from "../../common/title";
+import { bodyWeightMeterMeasures } from "../../../consts/measures/body_weight_meter_measures";
+import { useTwoOperatorsConverter } from "../../../hooks/useTwoOperatorsConverter";
+import ConversionTwoOperators from "../../common/conversionTwoOperators";
 
 const Discount = () => {
-    return <div>Discount</div>;
+    const initialState = ["100", "10"];
+    const methodOfCalculation = "discount";
+    const {
+        firstResult,
+        secondResult,
+        changeValue,
+        deleteLastResultSymbol,
+        changeActiveField,
+        activeField,
+        finalResult,
+        clearActiveResultValues
+    } = useTwoOperatorsConverter(
+        bodyWeightMeterMeasures,
+        initialState,
+        methodOfCalculation
+    );
+
+    return (
+        <div className="container-inner">
+            <Title title={"Скидка"} />
+            <ConversionTwoOperators
+                firstResultValue={firstResult}
+                secondResultValue={secondResult}
+                toggleActiveField={changeActiveField}
+                activeField={activeField}
+                methodOfCalculation={methodOfCalculation}
+                finalResult={finalResult}
+            />
+            <SimplePanel
+                changeValue={changeValue}
+                deleteAllSymbols={clearActiveResultValues}
+                deleteLastSymbol={deleteLastResultSymbol}
+            />
+        </div>
+    );
 };
 
 export default Discount;
